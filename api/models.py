3f35e6290a8f121ea66bc6dc13c92b8854b0ea34
Lorrany gonçalves <146742359+lorranygoncalves2002@users.noreply.github.com>
2025-12-03 16:16:02 -0300

    Update models.py
    uso da biblioteca decimal para garantir precisão.
    
    -criação da classe 'category'  com campo do nome, tamanho max de 100 char e definição do metodo de representação
    
    - Criação da classe  'Product' com definição dos campos: name, description, price e category, todos com seus respectivos tipos de campos e argumentos. por ultimo a definição de representação

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+# Category(name: CharField)
+#Product(name: Charfield, description: TextField, 
+#price: DecimalField (2 decimal places), 
+#category: ForeignKey(Category, on_delete=models.PROTECT))
+
+class category(models.Model): 
+    name = models.CharField(max_length=100, unique=true)
+
+    def__str__(self): return self.name
+
+class Product(models.model):
+    name = models.Charfield(max_length=255)
+    #descrição do produto sem caracteres máximos
+    description = models.TextField(blank=true)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(category, on_delete=models.PROTECT)
+    def__str__(self): return self.name
