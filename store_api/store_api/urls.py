e844c18b8cf2b03e40f60f916996102af870a836
Lorrany gonçalves <146742359+lorranygoncalves2002@users.noreply.github.com>
2025-12-03 17:54:48 -0300

    Update urls.py

--- store_api/store_api/urls.py
+++ store_api/store_api/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import categoryViewSet, ProductViewSet
+
+router = DefaultRouter()
+router.register(r'categories', categoryViewSet)
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(router.urls)),
 ]
